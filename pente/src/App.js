cc7aac27758b9533305d6a7e5e98229741eb980c
EthanBarlevy <98473724+EthanBarlevy@users.noreply.github.com>
2024-01-31 10:41:57 -0700

    added some comments for how we could do the project

--- pente/src/App.js
+++ pente/src/App.js
@@ -23,3 +23,21 @@ function App() {
 }
 
 export default App;
+/*
+  class for intersection
+    has one enum for empty, player one, player two
+
+  class for board
+    has a 2d array of intersections
+    has a list of players 
+    have a place function that does the logic for if the intersection is empty
+    another function for checking if we have a 5 in a row (called in 32)
+    another function for checking if we can capture  (called in 32)
+    another function for checking if the player won from capturing (called in 34)
+
+  class for player
+    all this has is an counter for how many pieces we have captrued 
+    we could also have a color that the player can choose if we have time
+
+  i guess we just have the board in the main window or something
+*/
